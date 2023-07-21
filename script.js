5e1ec416622562c5701b220b2112afe9836b3f5e
anupam101999 <100278900+anupam101999@users.noreply.github.com>
2023-07-22 04:49:06 +0530

    images fixes

--- script.js
+++ script.js
@@ -1,29 +1,40 @@
 // JavaScript code from script.js
-const slidesContainer = document.querySelector(".slides");
-const slides = slidesContainer.children;
-let slideIndex = 0;
+document.addEventListener("DOMContentLoaded", function () {
+    const slidesContainer = document.querySelector(".slides");
+    const slides = slidesContainer.children;
+    let slideIndex = 0;
 
-function showSlide(n) {
-    slideIndex = n;
-    slidesContainer.style.transform = `translateX(-${slideIndex * 100}%)`;
-}
-
-function nextSlide() {
-    slideIndex = (slideIndex + 1) % slides.length;
-    showSlide(slideIndex);
-}
-
-// Call the nextSlide function every 2 seconds (adjust this value as you like)
-setInterval(nextSlide, 2000);
-
-// Ensure the last image doesn't leave a blank page
-slidesContainer.addEventListener("transitionend", () => {
-    if (slideIndex === slides.length - 1) {
-        slidesContainer.style.transition = "none";
-        slideIndex = 0;
-        showSlide(slideIndex);
-        setTimeout(() => {
-            slidesContainer.style.transition = "transform 0.5s ease";
-        }, 0);
+    function showSlide(n) {
+        slideIndex = n;
+        slidesContainer.style.transform = `translateX(-${slideIndex * 100}%)`;
     }
+
+    function nextSlide() {
+        slideIndex = (slideIndex + 1) % slides.length;
+        showSlide(slideIndex);
+
+        // Set the manual image count based on screen width in inches (approximation)
+        const screenWidthInPixels = window.innerWidth;
+        const dpi = 96; // Standard DPI value for most screens (adjust if necessary)
+        const inches = screenWidthInPixels / dpi;
+
+        let manualImageCount;
+
+        if (inches < 10) { // For small screens (up to 4 inches wide)
+            manualImageCount = 11;
+        } else { // For large screens (more than 7 inches wide)
+            manualImageCount = 5;
+        }
+
+        if (slideIndex === manualImageCount) {
+            // Reset back to the first image after the manualImageCount
+            setTimeout(() => {
+                slideIndex = 0;
+                showSlide(slideIndex);
+            }, 2000);
+        }
+    }
+
+    // Call the nextSlide function every 2 seconds (adjust this value as you like)
+    setInterval(nextSlide, 2000);
 });
