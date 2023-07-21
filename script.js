24359fec2dd171b6ce4f1db1bb800eb5cb1dba63
anupam101999 <100278900+anupam101999@users.noreply.github.com>
2023-07-22 02:02:19 +0530

    add

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const images = document.querySelectorAll("#slider img");
-    let index = 0;
+// document.addEventListener("DOMContentLoaded", function() {
+//     const images = document.querySelectorAll("#slider img");
+//     let index = 0;
   
-    function showImage() {
-      images.forEach(image => image.style.display = "none");
-      images[index].style.display = "block";
-      index = (index + 1) % images.length;
-      setTimeout(showImage, 5000); // Change image every 5 seconds
-    }
+//     function showImage() {
+//       images.forEach(image => image.style.display = "none");
+//       images[index].style.display = "block";
+//       index = (index + 1) % images.length;
+//       setTimeout(showImage, 5000); // Change image every 5 seconds
+//     }
   
-    showImage();
-  });
+//     showImage();
+//   });
   
